d6c63a767e7c98c7f568d9af034c87ada3a163b1
hanuman119 <hanuma6305@gmail.com>
2026-02-26 15:06:11 +0530

    first commit

---
+++ frontend/my-app/src/App.jsx
@@ -0,0 +1,16 @@
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+function App() {
+  return (
+    <div>
+      <h1>My Auth App</h1>
+
+      <Login />
+      <hr />
+      <Register />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/my-app/src/components/Login.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("Login Data:", email, password);
+
+    // later connect to backend API
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br /><br />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
